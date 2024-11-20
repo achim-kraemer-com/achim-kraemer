2ae45b892e24ce68e2655118507979bbe311e64f
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2024-11-20 09:17:27 +0100

    main: deploy.sh corrected

--- application/deploy.sh
+++ application/deploy.sh
@@ -3,5 +3,5 @@ composer self-update
 composer install
 php bin/console doctrine:migrations:migrate -n
 php bin/console cache:clear
-yarn install
-yarn encore production
+npm install --production
+npm run build 
