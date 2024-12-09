414979b796420f4cb83ca5fcb4bf7e0e003d7e7c
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2024-12-09 21:06:33 +0100

    main: optiimised

--- application/tailwind.config.js
+++ application/tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeInLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-100px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        fadeInRight: {
+          '0%': { opacity: '0', transform: 'translateX(100px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        verticalLine: {
+          '0%': { height: '0%' },
+          '100%': { height: '100%' },
+        },
+      },
+      animation: {
+        'fade-in-left': 'fadeInLeft 1s ease-out forwards',
+        'fade-in-right': 'fadeInRight 1s ease-out forwards',
+        'vertical-line': 'verticalLine 1s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
