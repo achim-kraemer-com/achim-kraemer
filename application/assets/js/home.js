8f741d5fa2b8c857947753eb98852ab694712494
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2024-12-10 14:35:21 +0100

    main: animation and line optimised

--- application/assets/js/home.js
+++ application/assets/js/home.js
@@ -30,16 +30,21 @@ document.addEventListener("DOMContentLoaded", function () {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             const block = entry.target;
+            const line = block.querySelector('.block-line');
+            const blockHeight = block.offsetHeight;
             if (entry.isIntersecting) {
+                line.style.height = `${blockHeight}px`;
+
                 // Entferne die translate Klassen, um die Animation auszuführen
                 block.classList.remove('opacity-0', 'translate-y-10');
                 block.querySelector('.headline').classList.remove('translate-x-[-100%]');
-                block.querySelector('.block-line').classList.remove('translate-y-10');
+                line.classList.remove('translate-y-10');
                 block.querySelector('.text').classList.remove('translate-x-20');
             } else {
+                line.style.height = 0;
                 block.classList.add('opacity-0', 'translate-y-10');
                 block.querySelector('.headline').classList.add('translate-x-[-100%]');
-                block.querySelector('.block-line').classList.add('translate-y-10');
+                line.classList.add('translate-y-10');
                 block.querySelector('.text').classList.add('translate-x-20');
             }
         });
