08cdb0a06cc410e2f9b6aedb16e161eebb328673
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2025-01-18 14:04:27 +0100

    main: more information in contact mail

--- application/src/Controller/HomeController.php
+++ application/src/Controller/HomeController.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
@@ -24,17 +26,35 @@ class HomeController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             // Verarbeitung des Formulars, z.B. Senden einer E-Mail
             $data  = $form->getData();
-            $email = (new Email())
+            $email = (new TemplatedEmail())
                 ->from($data['email'])
                 ->to('kontakt@achim-kraemer.com')
                 ->subject('Contact Form Message')
-                ->text($data['message']);
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context(
+                    [
+                        'name'    => $data['name'],
+                        'phone'   => $data['phone'],
+                        'mail'    => $data['email'],
+                        'message' => $data['message'],
+                    ]
+                );
 
-            $mailer->send($email);
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+                $email = (new Email())
+                    ->from('kontakt@achim-kraemer.com')
+                    ->to('kontakt@achim-kraemer.com')
+                    ->subject('Es gab anscheinend einen Fehler beim Versenden einer KontaktMail')
+                    ->text('Es gab anscheinend einen Fehler beim Versenden einer KontaktMail: '.$e->getMessage());
+
+                $mailer->send($email);
+            }
 
             $this->addFlash('success', $translator->trans('app.contact.email_send'));
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirect($this->generateUrl('app_home').'#contact');
         }
 
         return $this->render('home/index.html.twig', [
